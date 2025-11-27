8d01a7f879e1228f0107df8cf56453c70d99f067
khoale2006 <khoa4122b@gmail.com>
2025-11-27 21:16:07 +0700

    Cap nhat bai tap Java 05

--- Java_05.java
+++ Java_05.java
@@ -1,8 +1,8 @@
 public class Java_05 {
     public static void main(String[] args) {
-
-        System.out.println("Hello World 1");
-        System.out.println("Hello World 2");
+        System.out.print("Hỏi Dân IT");
+        System.out.println("với Eric");
+        System.out.printf("có %.0f người đăng ký", 40000.55);
 
     }
 }
