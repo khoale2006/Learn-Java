e8de29e189d61308ae426a190d32834b9c9b3d68
khoale2006 <khoa4122b@gmail.com>
2025-11-27 20:53:55 +0700

    Up code Java

---
+++ Java_05.java
@@ -0,0 +1,8 @@
+public class Java_05 {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 9;
+        int c = a + b;
+        System.out.println("Sum: " + c);
+    }
+}
