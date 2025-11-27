21635f4bef48e706ae237b74f63323e548a88c05
khoale2006 <khoa4122b@gmail.com>
2025-11-27 21:09:25 +0700

    Cap nhat bai tap Java 05

--- Java_05.java
+++ Java_05.java
@@ -1,6 +1,8 @@
 public class Java_05 {
     public static void main(String[] args) {
-        int test = 5 + 9 * 3 - 10 / 2;
-        System.out.println("Sum: " + test);
+
+        System.out.println("Hello World 1");
+        System.out.println("Hello World 2");
+
     }
 }
