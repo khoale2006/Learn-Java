3709d8d45de6e7d3e199ecb2fa53917724a4b529
khoale2006 <khoa4122b@gmail.com>
2025-11-28 20:24:20 +0700

    Cap nhat bai tap Java 06

--- Java_06.java
+++ Java_06.java
@@ -4,12 +4,13 @@ public class Java_06 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Student's name: ");
         String studentName = scanner.nextLine();
         System.out.println("Student's mark: ");
         int studentMark = scanner.nextInt();
 
-        System.out.println("Student's information: " + studentName + " - " + studentMark);
+        System.out.println(studentName + " has mark : " + studentMark);
 
         scanner.close();
     }
